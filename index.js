d1a9a7a6c52cdf0806245dc92948d565f511aa32
destro-er <45981076+destro-er@users.noreply.github.com>
2022-08-18 09:40:19 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function greet(name, lastName) {
+    console.log('hello ' + name + ' ' + lastName);
+
+}
+
+function square(number) {
+    return number * number;
+    
+}
+
+
+console.log(square(2));
